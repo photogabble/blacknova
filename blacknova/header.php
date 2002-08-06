68ed8d6c00b9e80634a6dcb545a646ffa6e5a4b9
Ouroborus <ouroborus@users.sourceforge.net>
2002-08-06 00:43:38 +0000

    Cleaned up. Switched to external stylesheets. Added ability to specify additional stylesheets.

--- blacknova/header.php
+++ blacknova/header.php
@@ -1,64 +1,69 @@
-<? header("Cache-Control: no-cache, must-revalidate");
-// Comment out the line below if you are running php 4.0.6 or earlier
-ob_start("ob_gzhandler");
+<?
+// header.php
+//
+// outputs the game's standard beginning-of-document stuff
+// Control variables:
+//   $title - inserted into the <title> tag
+//   $stylesheet - if you need additional stylesheets, set this to the literal html to insert into the
+//                 document. try to use <link> instead of <style> tags
+//   $interface - generally pulled from the users cookie, currently determines
+//                if the user wants dhtml or not
+//   $no_body - set to TRUE if you want to output the <body> tag yourself
 
-?>
+header("Cache-Control: no-cache, must-revalidate");
+
+// Comment out the line below if you are running php 4.0.6 or earlier
+//ob_start("ob_gzhandler");
+
+$html_header = <<<EOD
 <!doctype html public "-//w3c//dtd html 3.2//en">
 <html>
 <head>
-<meta http-equiv="Pragma" content="no-cache">
-<title><? echo $title; ?></title>
- <style type="text/css">
- <!--
-<?
-if($interface == "")
-{
-  $interface = "main.php";
-}
-
-if($interface == "main.php")
-{ 
-  echo "  a.mnu {text-decoration:none; font-size: 8Pt; font-family: verdana; color:white; font-weight:bold;}
-  a.mnu:hover {text-decoration:none; font-size: 8Pt; font-family: verdana; color:#3366ff; font-weight:bold;}
-  div.mnu {text-decoration:none; font-size: 8Pt; font-family: verdana; color:white; font-weight:bold;}
-  span.mnu {text-decoration:none; font-size: 8Pt; font-family: verdana; color:white; font-weight:bold;}
-  a.dis {text-decoration:none; font-size: 8Pt; font-family: verdana; color:silver; font-weight:bold;}
-  a.dis:hover {text-decoration:none; font-size: 8Pt; font-family: verdana; color:#3366ff; font-weight:bold;}
-  table.dis {text-decoration:none; font-size: 8Pt; font-family: verdana; color:silver; font-weight:bold;}
-  table.dis:hover {text-decoration:none; font-size: 8Pt; font-family: verdana; color:#3366ff; font-weight:bold;}
-  .portcosts1 {width:7em;border-style:none;font-family: verdana;font-size:12pt;background-color:$color_line1;color:#c0c0c0;}
-  .portcosts2 {width:7em;border-style:none;font-family: verdana;font-size:12pt;background-color:$color_line2;color:#c0c0c0;}
-  .headlines {text-decoration:none; font-size:8Pt; font-family:verdana,Arial,san-serif; font-weight:bold; color:white;}
-  .headlines:hover {text-decoration:none; color:#3366ff;}
-  .faderlines {background-color:$color_line2;}
-  .nav          { text-decoration: none; }
-  .nav:link     { text-decoration: none; color: lime;}
-  .nav:visited  { text-decoration: none; color: lime;}
-  .nav:hover    { text-decoration: none; color: yellow;}
-";
-}
-echo "\n  body {font-family: Arial, Helvetica, sans-serif; font-size: x-small;}\n";
-?>
- -->
- </style>
+  <meta http-equiv="Pragma" content="no-cache">
+  <title>{$title}</title>
+  <link rel="stylesheet" href="default.css" type="text/css">
+  [stylesheet]
+  {$stylesheet}
 </head>
+[body]
+EOD;
 
-<?
+$html_stylesheet_main = '<link rel="stylesheet" href="main.css" type="text/css">';
 
-if(empty($no_body))
+$html_body_main = <<<EOD
+<body background="images/bgoutspace1.gif" bgcolor="#000000" text="#c0c0c0" link="#00ff00" vlink="#00ff00" alink="#ff0000">
+EOD;
 
-{
+$html_body_default = <<<EOD
+<body background="" bgcolor="#000000" text="#c0c0c0" link="#00ff00" vlink="#808080" alink="#ff0000">
+EOD;
 
-  if($interface=="main.php")
-  {
-  	echo "<body background=\"images/bgoutspace1.gif\" bgcolor=\"#000000\" text=\"#c0c0c0\" link=\"#00ff00\" vlink=\"#00ff00\" alink=\"#ff0000\">";
+if(isset($interface)) {
+  switch($interface) {
+    case 'maintext.php':
+      $html_header = str_replace('[stylesheet]','',$html_header);
+      break;
+    case 'main.php':
+    default:
+      $html_header = str_replace('[stylesheet]',$html_stylesheet_main,$html_header);
   }
-  else
-  {
-  	echo "<body background=\"\" bgcolor=\"#000000\" text=\"#c0c0c0\" link=\"#00ff00\" vlink=\"#808080\" alink=\"#ff0000\">";
-  }
-
+} else {
+  $interface = 'main.php';
+  $html_header = str_replace('[stylesheet]',$html_stylesheet_main,$html_header);
 }
-echo "\n";
+
+if(empty($no_body)) {
+  switch($interface) {
+    case 'main.php':
+      $html_header = str_replace('[body]',$html_body_main,$html_header);
+      break;
+    default:
+      $html_header = str_replace('[body]',$html_body_default,$html_header);
+  }
+} else {
+  $html_header = str_replace('[body]','',$html_header);
+}
+
+echo $html_header;
 
 ?>
