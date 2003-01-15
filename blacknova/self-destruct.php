dd71915909b17f58dfdb0c7c5fe260f82619e6ee
Adam Fort <i_m_sane@users.sourceforge.net>
2003-01-15 02:34:34 +0000

    a minor bugfix

--- blacknova/self-destruct.php
+++ blacknova/self-destruct.php
@@ -1,4 +1,6 @@
 <?
+// This self destruct code will only work with version 4.1 code
+
 include("config.php");
 updatecookie();
 
@@ -16,41 +18,98 @@ if(checklogin())
 
 bigtitle();
 
-$result = $db->Execute("SELECT ship_id,character_name FROM $dbtables[ships] WHERE email='$username'");
+$sure = $_POST['sure'];
+$pass = $_POST['pass'];
+
+$result = $db->Execute("SELECT ship_id,character_name,password FROM $dbtables[ships] WHERE email='$username'");
 $playerinfo = $result->fields;
 
-if(!isset($sure))
-{
-  echo "<FONT COLOR=RED><B>$l_die_rusure</B></FONT><BR><BR>";
-  echo "<A HREF=$interface>$l_die_nonono</A> $l_die_what<BR><BR>";
-  echo "<A HREF=self-destruct.php?sure=1>$l_yes!</A> $l_die_goodbye<BR><BR>";
+if ($sure == -1) {
+	echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0;URL=$interface?id=" . $playerinfo[ship_id] . "\">";
 }
-elseif($sure == 1)
-{
-  echo "<FONT COLOR=RED><B>$l_die_check</B></FONT><BR><BR>";
-  echo "<A HREF=$interface>$l_die_nonono</A> $l_die_what<BR><BR>";
-  echo "<A HREF=self-destruct.php?sure=2>$l_yes!</A> $l_die_goodbye<BR><BR>";
-}
-elseif($sure == 2)
-{
-  echo "$l_die_count<BR>";
-  echo "$l_die_vapor<BR><BR>";
-  echo "$l_die_please.<BR>";
-  db_kill_player($playerinfo['ship_id']);
-  cancel_bounty($playerinfo['ship_id']);
-  adminlog(LOG_ADMIN_HARAKIRI, "$playerinfo[character_name]|$ip");
-  playerlog($playerinfo[ship_id], LOG_HARAKIRI, "$ip");
-}
-else
-{
-  echo "$l_die_exploit<BR><BR>";
+
+
+if(!isset($sure)) {
+echo "
+<TABLE width=75% align=center>
+<TR>
+	<TD colspan=2>
+		<FONT COLOR=RED><B>$l_die_rusure</B></FONT>
+	</TD>
+</TR>
+<TR>
+	<TD align=center>
+		<FORM action=\"$PHP_SELF\" method=post>
+		<INPUT type=hidden name=sure value=-1>
+		<INPUT type=submit value=\"$l_die_nonono\">
+		</FORM>
+	</TD>
+	<TD align=center>
+		$l_die_what
+	</TD>
+</TR>
+<TR>
+	<TD align=center>
+		<FORM action=\"$PHP_SELF\" method=post>
+		<INPUT type=hidden name=sure value=1>
+		<INPUT type=submit value=\"$l_yes\">
+		</FORM>
+	</TD>
+	<TD align=center>
+		$l_die_goodbye
+	</TD>
+</TR>
+</TABLE>
+";
+} elseif(($sure == 1) || (($sure == 2) && ($pass != $playerinfo['password']))) {	
+echo "
+<TABLE width=75% align=center>
+<TR>
+	<TD colspan=2>
+	<FONT COLOR=RED><B>$l_die_rusure</B></FONT>
+	</TD>
+</TR>
+<TR>
+	<TD colspan=2 align=center>
+	You must enter your password to destroy your ship.
+";
+if ($sure == 2) { echo "<BR>Password was incorrect."; }
+echo "
+	<FORM action=\"$PHP_SELF\" method=post>
+	<INPUT type=hidden name=sure value=2>
+	Password: <INPUT type=password name=pass>
+	</TD>
+</TR>
+<TR>
+	<TD align=center>
+		<INPUT type=submit value=$l_yes>&nbsp;&nbsp;$l_die_goodbye
+		</FORM>
+	</TD>
+	<TD align=center>
+	<FORM action=\"$PHP_SELF\" method=post>
+	<INPUT type=hidden name=sure value=-1>
+	$l_die_what&nbsp;&nbsp;<INPUT type=submit value=$l_die_nonono>
+	</FORM>
+	</TD>
+</TR>
+</TABLE>
+";
+} elseif(($sure == 2) && ($pass == $playerinfo['password'])) { 
+	echo "$l_die_count<BR>";
+	echo "$l_die_vapor<BR><BR>";
+	echo "$l_die_please.<BR>";
+	db_kill_player($playerinfo['ship_id']);
+	cancel_bounty($playerinfo['ship_id']);
+	adminlog(LOG_ADMIN_HARAKIRI, "$playerinfo[character_name]|$ip");
+	playerlog($playerinfo[ship_id], LOG_HARAKIRI, "$ip");
+} elseif ($sure != -1) {
+	echo "$l_die_exploit<BR><BR>";
 }
 
 if($sure != 2)
 {
-  TEXT_GOTOMAIN();
+	TEXT_GOTOMAIN();
 }
 
 include("footer.php");
-
 ?>
