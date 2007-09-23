49060be86c2f3c6e3a4ebd98a585663201bea0d0
L. Patrick Smallwood <iamsure@users.sourceforge.net>
2007-09-23 12:46:52 +0000

    Change to non-aliased php function (potential performance, deprecation)

--- includes/timer.php
+++ includes/timer.php
@@ -36,8 +36,8 @@ class c_Timer
     {
         $start_u = substr($this->t_start,0,10); $start_s = substr($this->t_start,11,10);
         $stop_u  = substr($this->t_stop,0,10);  $stop_s  = substr($this->t_stop,11,10);
-        $start_total = doubleval($start_u) + $start_s;
-        $stop_total  = doubleval($stop_u) + $stop_s;
+        $start_total = floatval($start_u) + $start_s;
+        $stop_total  = floatval($stop_u) + $stop_s;
         $this->t_elapsed = $stop_total - $start_total;
         return $this->t_elapsed;
     }
