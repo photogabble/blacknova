b6f637ea8cc9b28f42688496250221f76101a8f4
L. Patrick Smallwood <iamsure@users.sourceforge.net>
2007-09-29 17:03:40 +0000

    Missing files.

---
+++ includes/num_energy.php
@@ -0,0 +1,23 @@
+<?php
+// Copyright (C) 2001 Ron Harwood and L. Patrick Smallwood
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// File: functions/num_energy.php
+
+function num_energy($level_power, $level_factor, $level_magnitude)
+{
+    return round(pow($level_factor, $level_power) * 5 * $level_magnitude);
+}
+?>
