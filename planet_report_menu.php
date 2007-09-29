b6f05bd6df787fe4340f37c44437c27effbba99a
L. Patrick Smallwood <iamsure@users.sourceforge.net>
2007-09-29 18:33:17 +0000

    Templating planet report menu. Further fixes needed (hardcoded english phrases)

--- planet_report_menu.php
+++ planet_report_menu.php
@@ -47,37 +47,20 @@ include_once ("./header.php");
 global $playerinfo;
 global $l_pr_teamlink, $ship_based_combat;
 global $l_pr_planetstatus, $l_pr_changeprods, $l_pr_comm_disp, $l_pr_prod_disp1, $l_pr_prod_disp2, $l_pr_baserequired, $l_pr_team_disp;
-
-echo "<h1>" . $title. "</h1>\n";
-
-echo "<strong><a href=\"standard_report.php\" name=\"Planet Status\">$l_pr_planetstatus</a></strong><br>" .
-     "$l_pr_comm_disp<br>" .
-     "<br>";
-
-if (!$ship_based_combat)
-{
-    echo "<strong><a href=\"planet_defense_report.php\" name=\"Planet Defense\">Planet Defenses</a></strong><br>" .
-         "Display the defense levels of your planets.<br>" .
-         "<br>";
-}
-
-echo "<strong><a href=\"planet_production_change.php\" name=\"Planet Status\">$l_pr_changeprods</a></strong> &nbsp;&nbsp; $l_pr_baserequired<br>" .
-     $l_pr_prod_disp1 . "<br>" . $l_pr_prod_disp2 . "<br>";
-
-if ($playerinfo['team']>0)
-{
-    echo "<br>" .
-         "<strong><a href=team_planets.php>$l_pr_teamlink</a></strong><br> " .
-         $l_pr_team_disp .
-         "<br>";
-    echo "<br>" .
-         "<strong><a href=team_defenses.php>Show Team Defenses</a></strong><br> Show the Defense Levels of all planets on your team.<br>";
-}
-
-echo "<br><br>";
-
 global $l_global_mmenu;
-echo "<a href=\"main.php\">" . $l_global_mmenu . "</a>";
 
+$template->assign("title", $title);
+$template->assign("l_pr_planetstatus", $l_pr_planetstatus);
+$template->assign("l_pr_comm_disp", $l_pr_comm_disp);
+$template->assign("ship_based_combat", $ship_based_combat);
+$template->assign("l_pr_changeprods", $l_pr_changeprods);
+$template->assign("l_pr_baserequired", $l_pr_baserequired);
+$template->assign("l_pr_prod_disp1", $l_pr_prod_disp1);
+$template->assign("l_pr_prod_disp2", $l_pr_prod_disp2);
+$template->assign("playerinfo_team", $playerinfo['team']);
+$template->assign("l_pr_team_disp", $l_pr_team_disp);
+$template->assign("l_pr_teamlink", $l_pr_teamlink);
+$template->assign("l_global_mmenu", $l_global_mmenu);
+$template->display("$templateset/planet_report_menu.tpl");
 include_once ("./footer.php");
 ?>
